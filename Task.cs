b6b367e76271c1892f6d6cae5fa5342992472117
ulyanaukolova <113050523+ulyanaukolova@users.noreply.github.com>
2022-10-20 22:34:03 +0300

    Create Task.cs

---
+++ Task.cs
@@ -0,0 +1,18 @@
+﻿namespace prctic4
+{
+    class Task
+    {
+        public Task(string name, string description, DateTime date)
+        {
+            this.name = name;
+            this.description = description;
+            this.date = date;
+        }
+
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime date { get; set; }
+
+    }
+
+}
