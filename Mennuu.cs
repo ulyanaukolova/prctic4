4eb7fb6f316532f1c9a232575af9b5bf7168f227
ulyanaukolova <113050523+ulyanaukolova@users.noreply.github.com>
2022-10-24 19:31:07 +0300

    Update Mennuu.cs

--- Mennuu.cs
+++ Mennuu.cs
@@ -1,77 +1,9 @@
-﻿namespace prctic4
+﻿namespace ToD0
 {
-    class Menu
+    internal class Note
     {
-        public int selectedIndex;
-        public List<Task> taskList;
-
-        public Menu(List<Task> taskList)
-        {
-            this.taskList = taskList;
-            selectedIndex = 0;
-        }
-
-        public void ShowMenu(DateTime date)
-        {
-            taskList = Program.ShowTasksByDate(date);
-
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine(date);
-                for (var i = 0; i < taskList.Count; i++)
-                {
-                    if (i == selectedIndex)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Magenta;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                    }
-
-                    Console.WriteLine($"{taskList[i].name}");
-                    if (i == selectedIndex)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                    }
-                }
-
-                var input = Console.ReadKey();
-
-                if (input.Key is ConsoleKey.LeftArrow)
-                {
-                    date = date.AddDays(-1);
-                    taskList = Program.ShowTasksByDate(date);
-                }
-
-                if (input.Key is ConsoleKey.RightArrow)
-                {
-                    date = date.AddDays(1);
-                    taskList = Program.ShowTasksByDate(date);
-                }
-
-                if (input.Key is ConsoleKey.DownArrow)
-                {
-                    if (selectedIndex < taskList.Count - 1) selectedIndex++;
-                }
-
-                if (input.Key is ConsoleKey.UpArrow)
-                {
-                    if (selectedIndex > 0) selectedIndex--;
-                }
-
-                if (input.Key is ConsoleKey.Enter)
-                {
-                    Console.Clear();
-                    Program.ViewDetails(selectedIndex);
-                    Console.ReadKey();
-                }
-
-                if (input.Key is ConsoleKey.Escape)
-                {
-                    Environment.Exit(0);
-                }
-            }
-        }
-
+        public string Name;
+        public string Description;
+        public DateTime EndDate;
     }
 }
